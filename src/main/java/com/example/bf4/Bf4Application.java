9e218539644a7fe16e6c5f43da44a0805b4e2cea
omar <vrtlvrtl89@gmail.com>
2023-01-17 12:12:57 +0100

    Initial commit

---
+++ src/main/java/com/example/bf4/Bf4Application.java
@@ -0,0 +1,13 @@
+package com.example.bf4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bf4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bf4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bf4/Bf4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bf4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bf4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
